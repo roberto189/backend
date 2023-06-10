85dcc5e5566ad63dee417c1eda7fb10ebda89474
roberto189 <126641489+roberto189@users.noreply.github.com>
2023-06-10 16:16:49 -0700

    cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-const cors = require ("cors")
+const cors = require("cors")
 
 const cwd = process.cwd();
 
