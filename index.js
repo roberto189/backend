f0cf346a97b45f1c3307b8f2251100548bb04241
roberto189 <126641489+roberto189@users.noreply.github.com>
2023-06-10 15:45:34 -0700

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const cors = require ("cors")
 
 const cwd = process.cwd();
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+app.use(cors())
 
 db.once('open', () => {
   app.listen(PORT, () => {
